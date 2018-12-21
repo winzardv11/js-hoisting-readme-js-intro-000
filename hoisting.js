1ec30609652d81bbad499ac49d709eb437022f70
winzard <ridzwan.midris@gmail.com>
2018-12-21 18:22:02 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,10 +1,12 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
   var lyric = "maybe";
+
 }
 
 
@@ -15,16 +17,22 @@ function crazy() {
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  // fix the code in here:
+  thisIsCrazy();
+
+
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
+  var name = "Kristin";
 
   sayMy();
 
   function sayMy() {
     console.log(name);
     var name = "Kristin";
+
   }
 }
